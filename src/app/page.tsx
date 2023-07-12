e6aeb48a72ca915d342f598e9e66b205cb99d6d0
George Satellite <lunaerxs@gmail.com>
2023-07-12 23:54:30 +0300

    +2 terms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,11 @@ const IndexPage = () => {
 					{/* TODO: link to "sampling" */}
 					<TermRow>Converts a continuous time signal to a digital signal by sampling.</TermRow>
 				</Term>
-				<Term id='adsr' term='ADSR' expansion='Attack Decay Sustain Release'>
+				<Term id='adsr' term='ADSR' expansion='Attack / Decay / Sustain / Release'>
 					{/* eslint-disable-next-line no-warning-comments */}
 					{/* TODO: link to "envelope" */}
 					<TermRow>By default this refers to a volume envelope.</TermRow>
 					<TermRow>Note that attack/decay/release are times and sustain is a level.</TermRow>
-					<TermRow>Mucho confusing for n00bs, especially when laid out such as on the Juno 60 where they are identical looking sliders.</TermRow>
 				</Term>
 				<Term id='aliasing' term='Aliasing'>
 					{/* eslint-disable-next-line no-warning-comments */}
@@ -27,6 +26,16 @@ const IndexPage = () => {
 					<TermRow>A set of samples could technically describe more than one signal. These are referred to as aliases, which is where the name comes from.</TermRow>
 					<TermRow>The common occurrence of this is trying to represent a signal higher than your nyquist frequency.</TermRow>
 					<TermExternalLink href='http://www.slack.net/~ant/bl-synth/3.nyquist.html'>The Nyquist Limit</TermExternalLink>
+				</Term>
+				<Term id='dac' term='DAC' expansion='Digital to Analog Converter'>
+					<TermRow>Converts a digital signal to a continuous time, analog signal.</TermRow>
+					{/* eslint-disable-next-line no-warning-comments */}
+					{/* TODO: link to "ADC" */}
+					<TermRow>The opposite of an ADC.</TermRow>
+				</Term>
+				<Term id='daw' term='DAW' expansion='Digital Audio Workstation'>
+					<TermRow>An audio or music program that lets you run plugins and arrange multiple audio or MIDI tracks.</TermRow>
+					<TermRow>Ableton Live or Logic Pro.</TermRow>
 				</Term>
 			</div>
 		</div>
