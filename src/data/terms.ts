ff3688e9a0e5fc1d66a0b98c2f63786ed7e632f2
George Satellite <lunaerxs@gmail.com>
2023-07-13 19:58:09 +0300

    improve: omega in braces

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -62,7 +62,7 @@ export const terms: Term[] = [
 		id: 'angular-frequency',
 		title: 'Angular Frequency',
 		paragraphs: [
-			'omega ω',
+			'omega (ω)',
 			'ω is used to represent radians per time unit, either per second or per sample depending on context.',
 			'When dealing with seconds, it\'s a factor of 2π larger than Hertz (1Hz = 2πω).',
 		],
