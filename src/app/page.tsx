6161574955db0cd9de4650dfc385fc4b48d301d8
George Satellite <lunaerxs@gmail.com>
2023-07-13 14:14:39 +0300

    data: terms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import {Term, TermRow, TermExternalLink} from '@/components/Term';
+import {terms} from '@/data/terms';
 
 const IndexPage = () => {
 	return (
@@ -6,45 +7,22 @@ const IndexPage = () => {
 			<h1 className='pb-2 text-5xl font-bold md:relative md:-left-1 md:text-7xl'>DSP Terms</h1>
 			<p className='text-xl'>Digital signal processing terminology.</p>
 			<div className='flex flex-col gap-10 pt-20 md:grid md:grid-cols-2 md:pt-28 lg:grid-cols-3'>
-				<Term id='adc' term='ADC' expansion='Analog to Digital Converter'>
-					<TermRow>Converts a continuous time signal to a digital signal by sampling.</TermRow>
-				</Term>
-				<Term id='adsr' term='ADSR' expansion='Attack / Decay / Sustain / Release'>
-					<TermRow>By default this refers to a volume envelope.</TermRow>
-					<TermRow>Note that attack/decay/release are times and sustain is a level.</TermRow>
-				</Term>
-				<Term id='aliasing' term='Aliasing'>
-					<TermRow>A type of distortion that results when reconstructing an analog signal from samples.</TermRow>
-					<TermRow>A set of samples could technically describe more than one signal. These are referred to as aliases, which is where the name comes from.</TermRow>
-					<TermRow>The common occurrence of this is trying to represent a signal higher than your nyquist frequency.</TermRow>
-					<TermExternalLink href='http://www.slack.net/~ant/bl-synth/3.nyquist.html'>The Nyquist Limit</TermExternalLink>
-				</Term>
-				<Term id='all-pass' term='All-pass'>
-					<TermRow>Formally a filter, but it doesn&apos;t change the frequency content, only the phase.</TermRow>
-					<TermRow>All frequencies pass through unaffected, hence the name &quot;all pass.&quot;</TermRow>
-					<TermRow>A building block used in reverbs.</TermRow>
-					<TermRow>It&apos;s made by adding a feedforward delay and then subtracting a feedback delay — both using the same coefficients.</TermRow>
-					<TermRow>It was invented specifically to create a digital delay that doesn&apos;t impact the frequency response of a signal, like a comb filter does.</TermRow>
-				</Term>
-				<Term id='amplitude' term='Amplitude'>
-					<TermRow>Peak value of a sinusoid.</TermRow>
-					<TermRow>Can be positive or negative, it&apos;s just &quot;how far from 0&quot;.</TermRow>
-				</Term>
-				<Term id='dac' term='DAC' expansion='Digital to Analog Converter'>
-					<TermRow>Converts a digital signal to a continuous time, analog signal.</TermRow>
-					<TermRow>The opposite of an ADC.</TermRow>
-				</Term>
-				<Term id='daw' term='DAW' expansion='Digital Audio Workstation'>
-					<TermRow>An audio or music program that lets you run plugins and arrange multiple audio or MIDI tracks.</TermRow>
-					<TermRow>Ableton Live or Logic Pro.</TermRow>
-				</Term>
-				<Term id='psola' term='PSOLA' expansion='Pitch Synchronous Overlap and Add'>
-					<TermRow>Speech synthesis technique invented in 1986 that chops audio into overlapping chunks. The distance between chunks can be increased or decreased to change pitch or repeated/deleted to change duration.</TermRow>
-					<TermExternalLink href='https://wiki.aalto.fi/pages/viewpage.action?pageId=155477136'>PSOLA - Aalto University Wiki</TermExternalLink>
-				</Term>
-				<Term id='simd' term='SIMD' expansion='Single instruction, multiple data.'>
-					<TermRow>A type of optimized CPU instruction that operates on a vector instead of a single piece of data.</TermRow>
-				</Term>
+				{terms.map(({id, term, expansion, paragraphs, externalLinks}) => {
+					return (
+						<Term key={id} id={id} term={term} expansion={expansion}>
+							{paragraphs.map(paragraph => {
+								return (
+									<TermRow key={paragraph}>{paragraph}</TermRow>
+								);
+							})}
+							{externalLinks?.map(({href, label}) => {
+								return (
+									<TermExternalLink key={href} href={href}>{label}</TermExternalLink>
+								);
+							})}
+						</Term>
+					);
+				})}
 			</div>
 		</div>
 	);
---
+++ src/data/terms.ts
@@ -0,0 +1,108 @@
+type Term = {
+	id: string;
+	term: string;
+	expansion?: string;
+	paragraphs: string[];
+	// eslint-disable-next-line no-warning-comments
+	externalLinks?: TermExternalLink[]; // TODO: rename to 'links'
+};
+
+type TermExternalLink = {
+	href: string;
+	// eslint-disable-next-line no-warning-comments
+	label: string; // TODO: rename to 'title'
+};
+
+export const terms: Term[] = [
+	{
+		id: 'adc',
+		term: 'ADC',
+		expansion: 'Analog to Digital Converter',
+		paragraphs: [
+			'Converts a continuous time signal to a digital signal by sampling.',
+		],
+	},
+	{
+		id: 'arsr',
+		term: 'ADSR',
+		expansion: 'Attack / Decay / Sustain / Release',
+		paragraphs: [
+			'By default this refers to a volume envelope.',
+			'Note that attack/decay/release are times and sustain is a level.',
+		],
+	},
+	{
+		id: 'aliasing',
+		term: 'Aliasing',
+		paragraphs: [
+			'A type of distortion that results when reconstructing an analog signal from samples.',
+			'A set of samples could technically describe more than one signal. These are referred to as aliases, which is where the name comes from.',
+			'The common occurrence of this is trying to represent a signal higher than your nyquist frequency.',
+		],
+		externalLinks: [
+			{
+				href: 'http://www.slack.net/~ant/bl-synth/3.nyquist.html',
+				label: 'The Nyquist Limit',
+			},
+		],
+	},
+	{
+		id: 'all-pass',
+		term: 'All-pass',
+		paragraphs: [
+			'Formally a filter, but it doesn\'t change the frequency content, only the phase.',
+			'All frequencies pass through unaffected, hence the name "all-pass".',
+			'A building block used in reverbs.',
+			'It\'s made by adding a feedforward delay and then subtracting a feedback delay — both using the same coefficients.',
+			'It was invented specifically to create a digital delay that doesn\'t impact the frequency response of a signal, like a comb filter does.',
+		],
+	},
+	{
+		id: 'amplitude',
+		term: 'Amplitude',
+		paragraphs: [
+			'Peak value of a sinusoid.',
+			'Can be positive or negative, it\'s just "how far from 0".',
+		],
+	},
+	{
+		id: 'dac',
+		term: 'DAC',
+		expansion: 'Digital to Analog Converter',
+		paragraphs: [
+			'Converts a digital signal to a continuous time, analog signal.',
+			'The opposite of an ADC.',
+		],
+	},
+	{
+		id: 'daw',
+		term: 'DAW',
+		expansion: 'Digital Audio Workstation',
+		paragraphs: [
+			'An audio or music program that lets you run plugins and arrange multiple audio or MIDI tracks.',
+			'Examples: Ableton Live, Logic Pro, etc.',
+		],
+	},
+	{
+		id: 'psola',
+		term: 'PSOLA',
+		expansion: 'Pitch Synchronous Overlap and Add',
+		paragraphs: [
+			'Speech synthesis technique invented in 1986 that chops audio into overlapping chunks. The distance between chunks can be increased or decreased to change pitch or repeated/deleted to change duration.',
+		],
+		externalLinks: [
+			{
+				href: 'https://wiki.aalto.fi/pages/viewpage.action?pageId=155477136',
+				label: 'PSOLA - Aalto University Wiki',
+			},
+		],
+	},
+	{
+		id: 'simd',
+		term: 'SIMD',
+		expansion: 'Single instruction, multiple data.',
+		paragraphs: [
+			'A type of optimized CPU instruction that operates on a vector instead of a single piece of data.',
+		],
+	},
+];
