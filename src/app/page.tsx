780f7a0aeab4d93a20eba00ad8b59d6b7d4607ca
George Satellite <lunaerxs@gmail.com>
2023-07-13 20:13:37 +0300

    remove grid columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function IndexPage() {
 		<div className='px-8 py-16 md:px-12 md:py-20 lg:px-20'>
 			<h1 className='pb-2 text-5xl font-bold md:relative md:-left-1 md:text-7xl'>DSP Terms</h1>
 			<p className='text-xl'>Digital signal processing terminology.</p>
-			<div className='flex flex-col gap-10 pt-20 md:grid md:grid-cols-2 md:pt-28 lg:grid-cols-3'>
+			<div className='flex flex-col gap-10 pt-20 md:pt-28'>
 				{terms.map(term => (<Term key={term.id} {...term}/>))}
 			</div>
 		</div>
