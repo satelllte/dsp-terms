9eee0053193cb72c6e4df104a5e372ea5250b38a
George Satellite <lunaerxs@gmail.com>
2023-07-13 21:16:52 +0300

    term: block-diagram

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -132,6 +132,15 @@ export const terms: Term[] = [
 		],
 	},
 	{
+		id: 'block-diagram',
+		title: 'Block diagram',
+		paragraphs: [
+			'High level visual description of how a signal flows through a system.',
+			'It\'s a rough sketch to give you a general idea of what\'s going on.',
+			'It\'s NOT a detailed schematic diagram which uses standardized symbolic representations of components or a circuit diagram (which shows electrical connections).',
+		],
+	},
+	{
 		id: 'dac',
 		title: 'DAC',
 		expansion: 'Digital to Analog Converter',
