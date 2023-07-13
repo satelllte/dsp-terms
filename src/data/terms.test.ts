275f22e489e3000ba148980b0e7e4a05cee55a2a
George Satellite <lunaerxs@gmail.com>
2023-07-13 20:36:24 +0300

    tests: compare against lower case

--- src/data/terms.test.ts
+++ src/data/terms.test.ts
@@ -4,14 +4,14 @@ import {terms} from './terms';
 describe('terms', () => {
 	they('are sorted', () => {
 		for (let i = 1; i < terms.length; i++) {
-			expect(terms[i - 1].id < terms[i].id).toEqual(true);
-			expect(terms[i - 1].title < terms[i].title).toEqual(true);
+			expect(terms[i - 1].id.toLowerCase() < terms[i].id.toLowerCase()).toEqual(true);
+			expect(terms[i - 1].title.toLowerCase() < terms[i].title.toLowerCase()).toEqual(true);
 		}
 	});
 
 	they('have no duplicates', () => {
-		const ids = terms.map(({id}) => id);
-		const titles = terms.map(({title}) => title);
+		const ids = terms.map(({id}) => id.toLowerCase());
+		const titles = terms.map(({title}) => title.toLowerCase());
 		expect((new Set(ids)).size).toEqual(ids.length);
 		expect((new Set(titles)).size).toEqual(titles.length);
 	});
