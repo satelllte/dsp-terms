45f1e1b30d722e786d80b9f3ef9410e8b58e1b80
George Satellite <lunaerxs@gmail.com>
2023-07-13 22:02:52 +0300

    page title, subtitle improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import {terms} from '@/data/terms';
 export default function IndexPage() {
 	return (
 		<div className='px-8 py-16 md:px-12 md:py-20 lg:px-20'>
-			<h1 className='pb-2 text-5xl font-bold md:relative md:-left-1 md:text-7xl'>DSP Terms</h1>
-			<p className='text-xl'>Digital signal processing terminology.</p>
+			<h1 className='relative -left-0.5 pb-2 text-5xl font-bold md:-left-1 md:text-7xl'>DSP Terms</h1>
+			<p className='text-base md:text-xl'>Digital signal processing terminology.</p>
 			<div className='flex flex-col gap-9 pt-12 md:gap-12 md:pt-20'>
 				{terms.map(term => (<Term key={term.id} {...term}/>))}
 			</div>
