c07eb5f51e1c9ce23b1d09b298716d07ad7a2cf9
George Satellite <lunaerxs@gmail.com>
2023-07-13 21:00:31 +0300

    improve tests

--- src/data/terms.test.ts
+++ src/data/terms.test.ts
@@ -1,18 +1,29 @@
-import {describe, expect, it as they} from 'vitest';
+import {describe, expect, it} from 'vitest';
 import {terms} from './terms';
 
 describe('terms', () => {
-	they('are sorted', () => {
-		for (let i = 1; i < terms.length; i++) {
-			expect(terms[i - 1].id.toLowerCase() < terms[i].id.toLowerCase()).toEqual(true);
-			expect(terms[i - 1].title.toLowerCase() < terms[i].title.toLowerCase()).toEqual(true);
+	const they = it;
+
+	const ids = terms.map(({id}) => id.toLowerCase());
+	const titles = terms.map(({title}) => title.toLowerCase());
+
+	they('have all ids sorted', () => {
+		for (let i = 1; i < ids.length; i++) {
+			expect(ids[i - 1] < ids[i]).toEqual(true);
 		}
 	});
 
-	they('have no duplicates', () => {
-		const ids = terms.map(({id}) => id.toLowerCase());
-		const titles = terms.map(({title}) => title.toLowerCase());
+	they('have all titles sorted', () => {
+		for (let i = 1; i < titles.length; i++) {
+			expect(titles[i - 1].toLowerCase() < titles[i].toLowerCase()).toEqual(true);
+		}
+	});
+
+	they('have no duplicates in ids', () => {
 		expect((new Set(ids)).size).toEqual(ids.length);
+	});
+
+	they('have no duplicates in titles', () => {
 		expect((new Set(titles)).size).toEqual(titles.length);
 	});
 });
