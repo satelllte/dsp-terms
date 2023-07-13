97ee406d7bd6c1635f4c306d74a813a28bb199ea
George Satellite <lunaerxs@gmail.com>
2023-07-13 19:48:03 +0300

    term: analog-modelling

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -51,6 +51,14 @@ export const terms: Term[] = [
 		],
 	},
 	{
+		id: 'analog-modelling',
+		title: 'Analog Modelling',
+		paragraphs: [
+			'Digital algorithms that simulate the behavior of electronic circuits (resistors, capacitors, op amps).',
+			'There are various flavors of white box (literal model of each component and their interactions) and black box (modelling the response).',
+		],
+	},
+	{
 		id: 'dac',
 		title: 'DAC',
 		expansion: 'Digital to Analog Converter',
